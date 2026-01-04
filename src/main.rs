4face99322569eac416537f9b51b1aa6e64e06e6
Matthew <matt@p-cone.com>
2026-01-04 15:11:59 -0500

    testing speech rec with kalosm:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::StreamExt;
+use kalosm_sound::*;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let model = Whisper::new().await?;
+    let mic = MicInput::default();
+
+    let stream = mic.stream();
+
+    println!("I should be listening now");
+    let mut text_stream: ChunkedTranscriptionTask<_> = stream.transcribe(model);
+
+    text_stream.next().await.unwrap().text();
+
+    Ok(())
 }
