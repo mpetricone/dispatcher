6be826762ec49ee98a05b002129a47bf2ae37376
Matthew Petricone <strstrea@gmail.com>
2026-01-04 15:48:57 -0500

    working transcription, but far too slow with whisper

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use futures_util::StreamExt;
-use kalosm_sound::*;
+use kalosm_sound::{rodio::Source, *};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let model = Whisper::new().await?;
     let mic = MicInput::default();
 
     let stream = mic.stream();
 
-    println!("I should be listening now");
-    let mut text_stream: ChunkedTranscriptionTask<_> = stream.transcribe(model);
+    let vad = stream.voice_activity_stream();
+    let mut audio_chunks = vad.rechunk_voice_activity();
 
-    text_stream.next().await.unwrap().text();
+    while let Some(input) = audio_chunks.next().await {
+        println!(
+            "New voice activity chunk wityh duration {:?}",
+            input.total_duration()
+        );
+        let mut ts = input.transcribe(Whisper::new().await?);
+
+        while let Some(text) = ts.next().await {
+            println!("{}", text.text());
+        }
+    }
 
     Ok(())
 }
