5905ec66ff5e07c0aabf65e596d7540ac713daf6
Matthew Petricone <strstrea@gmail.com>
2026-01-13 11:47:10 -0500

    input recorder working

--- src/input_recorder.rs
+++ src/input_recorder.rs
@@ -36,25 +36,28 @@ impl InputEvent {
 pub fn normalize_sequence(raw_seq: Arc<Mutex<Vec<rdev::Event>>>) -> Result<Vec<InputEvent>,String> {
     if let Ok(guard) = &raw_seq.lock() {
         let mut event_chain: Vec<InputEvent> = vec!();
-        // Meh should be a fairly short list
         let mut open_events: Vec<InputEvent> = vec!();
         let mut index = 0;
 
-        while index <= guard.len() {
+        while index < guard.len() {
             let cur = &guard[index];
             let duration = 0u32;
             match cur.event_type {
                 KeyPress(e) => {
-                    let existing = open_events.iter().position(|x| x.event_type == KeyPress(e));
-                    if open_events[existing].time > cur.time {
+                    let mut existing_idx = 0usize;
+                    if open_events.is_empty() {
+                        open_events.push(InputEvent::new(guard[index].event_type, None, guard[index].time));
+                    }
+                    existing_idx = open_events.iter().position(|x| x.event_type == KeyPress(e)).unwrap_or(0);
+                    if open_events[existing_idx].time <= cur.time {
                         if let Some(np) = &guard[index..].iter().position(|x| x.event_type == KeyRelease(e)) {
                             open_events.push(InputEvent::new(KeyPress(e), None, guard[index+np].time));
-                            event_chain.push(InputEvent::new(KeyPress(e),Some(guard[np+index].time.duration_since(cur.time).unwrap()), guard[np+index].time ));
+                            event_chain.push(InputEvent::new(KeyPress(e),Some(guard[np+index].time.duration_since(cur.time).unwrap()), guard[index].time ));
                         } else {
-                            eprintln!("Missing Releae for {:?} at index: {}", cur, index)
+                            event_chain.push(InputEvent::new(KeyPress(e), None, guard[index].time))
                         }
                     }
-                }
+                },
                 KeyRelease(e) => {
                     // I think there should only ever be 1
                     open_events.remove(
