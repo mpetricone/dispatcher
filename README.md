3207079454e6ad9ec4b32c48f254ca2ac19c1e78
Matthew Petricone <strstrea@gmail.com>
2026-01-23 00:14:00 -0500

    Documentation updated for file IO

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Voice Command dispatcher
 ## Status
 Currently, I am experimenting with crates to find the best options for voice transcription and kb/mouse input/output
 
+Please consult the rustdoc for up to date details.
+
 ### Currently using
 - Vosk -> Voice Recognition
 - voice-stream -> Voice Detection
--- src/file_io.rs
+++ src/file_io.rs
@@ -1,10 +1,15 @@
+//! File IO
+//! We'll be storing to json whenever possible
 use serde;
 use serde::de;
 use serde_json;
 use std::error::Error;
 use std::fs::OpenOptions;
 use std::io::{BufReader, BufWriter};
-
+///
+/// This is designed to read any struct implementing [serde::Deserialize]
+/// from file in json format.
+///
 pub fn from_file<T: de::DeserializeOwned>(file_path: &str) -> Result<T, Box<dyn Error>> {
     let fh = OpenOptions::new().read(true).write(false).open(file_path)?;
     let bufr = BufReader::new(fh);
@@ -12,6 +17,9 @@ pub fn from_file<T: de::DeserializeOwned>(file_path: &str) -> Result<T, Box<dyn 
     Ok(ret_v)
 }
 
+/// Writes any struct implementing [serde::Serialize] to file, with the option to truncate.
+///
+/// to_file will always attempt to create a file if none exists.
 pub fn to_file<T: serde::Serialize>(
     file_path: &str,
     truncate: bool,
